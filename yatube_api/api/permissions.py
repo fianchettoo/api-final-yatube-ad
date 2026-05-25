cba33b79aded616261c7c4c14cec4c1883ecaeab
fianchettoo <yegor.shvets.shust@gmail.com>
2026-05-25 18:35:35 +0300

    second commit

---
+++ yatube_api/api/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission позволяет изменять контент только авторам.
+    """
+    message = 'Изменение чужого контента запрещено!'
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
